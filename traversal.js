0b408072d84fc223168067be5185839b00d569ae
Nari Lee <narilee91@gmail.com>
2023-08-12 01:48:55 +0900

    implement depth-first traversal for binary search tree

--- traversal.js
+++ traversal.js
@@ -20,6 +20,30 @@ function breadthFirstSearch(tree) {
   }
 }
 
+function preOrder(node) {
+  if (node) {
+    console.log(node.value);
+    preOrder(node.left);
+    preOrder(node.right);
+  }
+}
+
+function inOrder(node) {
+  if (node) {
+    inOrder(node.left);
+    console.log(node.value);
+    inOrder(node.right);
+  }
+}
+
+function postOrder(node) {
+  if (node) {
+    postOrder(node.left);
+    postOrder(node.right);
+    console.log(node.value);
+  }
+}
+
 const bst = new BinarySearchTree();
 bst.insert(6);
 bst.insert(10);
@@ -30,3 +54,7 @@ bst.insert(30);
 bst.insert(1);
 
 breadthFirstSearch(bst); // bfs 실행결과: 6 3 10 1 9 30 8
+
+preOrder(bst.root); // dfs preOrder 실행결과: 6 3 1 10 9 8 30
+inOrder(bst.root); // dfs inOrder 실행결과: 1 3 6 8 9 10 30 (오름차순)
+postOrder(bst.root); // dfs postOrder 실행결과: 1 3 8 9 30 10 6
